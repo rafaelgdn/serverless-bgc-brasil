114478633c7f41953a060ddbd26f93309d7192c2
Rafael de Carvalho <rafaeldecarvalho.ps@gmail.com>
2020-08-22 16:39:10 -0300

    feat: add log-email to DynamoDB

--- serverless-api/sendMail.js
+++ serverless-api/sendMail.js
@@ -1,7 +1,52 @@
 import { transporter } from "./config/nodemailer-config";
+import AWS from "aws-sdk";
+import * as uuid from "uuid";
+
+AWS.config.update({ region: "us-east-2" });
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
 export function main(event, context, callback) {
-  const { email } = JSON.parse(event.body);
+  const { name, email, minion, quantity } = JSON.parse(event.body);
+
+  for (const value of [name, email, minion, quantity]) {
+    if (!value) {
+      callback(null, {
+        statusCode: 400,
+        body: `Missing param error`
+      });
+      return;
+    }
+  }
+
+  const params = {
+    TableName: process.env.tableName,
+    Item: {
+      logId: uuid.v1(),
+      name,
+      email,
+      minion,
+      quantity,
+      createdAt: Date.now()
+    }
+  };
+
+  dynamoDb.put(params, (error, data) => {
+    const headers = {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Credentials": true
+    };
+
+    if (error) {
+      const response = {
+        statusCode: 500,
+        headers: headers,
+        body: JSON.stringify({ error: error })
+      };
+      callback(null, response);
+      return;
+    };
+  });
+
   transporter.sendMail({
     from: "Rafael de Carvalho <rafaeldecarvalho.ps@gmail.com>",
     to: email,
@@ -9,8 +54,14 @@ export function main(event, context, callback) {
     text: "Teste",
     html: "<b>TESTE</b>"
   }).then(message => {
-    console.log(message);
+    callback(null, {
+      statusCode: 200,
+      body: message
+    });
   }).catch(err => {
-    console.log(err);
+    callback(null, {
+      statusCode: 500,
+      body: err
+    });
   });
 }
