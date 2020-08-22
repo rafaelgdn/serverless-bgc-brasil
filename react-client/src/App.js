6069924761ab6227c1736ff884a36d30da99de65
Rafael Carvalho <rafaeldecarvalho.ps@gmail.com>
2020-08-21 23:36:08 -0300

    feat: add header and first section at landing page

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -1,24 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
+import Minion from './assets/minion.png'
+import MinionSection1 from './assets/minion-section-1.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStar, faStarHalfAlt, faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <div className="minion-img">
+          <img src={Minion} />
+        </div>
+        <div>
+          <h1>Minion's Action Figure</h1>
+          <p>Produto extremamente realista, repleto de detalhes em feições, cores de roupas e articulações</p>
+          <div className="star-svg">
+            <FontAwesomeIcon icon={faStar} />
+            <FontAwesomeIcon icon={faStar} />
+            <FontAwesomeIcon icon={faStar} />
+            <FontAwesomeIcon icon={faStar} />
+            <FontAwesomeIcon icon={faStarHalfAlt} />
+          </div>
+          <button>
+            <FontAwesomeIcon icon={faShoppingCart} />
+            <span>Compre agora</span>
+          </button>
+        </div>
       </header>
+      <section>
+        <div className="section-1">
+          <h3>Sobre a Action Figure</h3>
+          <h2>Um Minion pode fazer a alegria do seu dia.</h2>
+          <p>Eles são fofos, desastrados, falam uma língua engraçada e são
+             as criaturinhas mais amadas do cinema e da internet nos últimos tempos.</p>
+        </div>
+        <div>
+          <img src={MinionSection1} />
+        </div>
+      </section>
     </div>
   );
 }
