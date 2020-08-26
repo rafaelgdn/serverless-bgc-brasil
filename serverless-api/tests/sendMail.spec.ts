497e6911100871617439fc5fa01002a4ad6de4f2
Rafael de Carvalho <rafaeldecarvalho.ps@gmail.com>
2020-08-26 12:15:17 -0300

    test: ensure sendMail calls callback with statusCode 400 if no minion is provided

--- serverless-api/tests/sendMail.spec.ts
+++ serverless-api/tests/sendMail.spec.ts
@@ -14,17 +14,28 @@ const requestWithoutName = {
   body: '{"email":"rafaeldecarvalho.ps@gmail.com", "minion":"kevin", "quantity":200}'
 }
 
+const requestWithoutMinion = {
+  body: '{"name":"Rafael", "email":"rafaeldecarvalho.ps@gmail.com", "quantity":200}'
+}
+
 describe('sendMail main function', () => {
-  test('Should return 400 if no email is provided', () => {
+  test('Should calls callback with statusCode 400 if no email is provided', () => {
     const callbackSpy = jest.spyOn(Stubs, 'callbackStub')
     sendMail.main(requestWithoutEmail, {}, Stubs.callbackStub)
     expect(callbackSpy).toHaveBeenCalledWith(null, expect.objectContaining({
       statusCode: 400
     }))
   })
-  test('Should return 400 if no name is provided', () => {
+  test('Should calls callback with statusCode 400 if no name is provided', () => {
     const callbackSpy = jest.spyOn(Stubs, 'callbackStub')
     sendMail.main(requestWithoutName, {}, Stubs.callbackStub)
+    expect(callbackSpy).toHaveBeenCalledWith(null, expect.objectContaining({
+      statusCode: 400
+    }))
+  })
+  test('Should calls callback with statusCode 400 if no minion is provided', () => {
+    const callbackSpy = jest.spyOn(Stubs, 'callbackStub')
+    sendMail.main(requestWithoutMinion, {}, Stubs.callbackStub)
     expect(callbackSpy).toHaveBeenCalledWith(null, expect.objectContaining({
       statusCode: 400
     }))
